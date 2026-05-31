aff9f969463fee963c7ed6372b26ef5bea08a091
crpier42 <crpier42@gmail.com>
2026-05-31 19:46:09 +0300

    Simplify public facade stub imports

--- snekql/__init__.pyi
+++ snekql/__init__.pyi
@@ -1,151 +1,50 @@
 from snekql.errors import (
-    DatabaseClosedError as DatabaseClosedError,
+    DatabaseClosedError,
+    DatabaseCloseTimeoutError,
+    DatabaseClosingError,
+    DatabaseRuntimeError,
+    ExecutionError,
+    FrozenModelError,
+    ModelDeclarationError,
+    ModelError,
+    ModelValidationError,
+    PoolTimeoutError,
+    QueryCompilationError,
+    QueryConstructionError,
+    QueryError,
+    SchemaError,
+    SchemaVerificationError,
+    SnekqlError,
+    TransactionClosedError,
 )
-from snekql.errors import (
-    DatabaseCloseTimeoutError as DatabaseCloseTimeoutError,
-)
-from snekql.errors import (
-    DatabaseClosingError as DatabaseClosingError,
-)
-from snekql.errors import (
-    DatabaseRuntimeError as DatabaseRuntimeError,
-)
-from snekql.errors import (
-    ExecutionError as ExecutionError,
-)
-from snekql.errors import (
-    FrozenModelError as FrozenModelError,
-)
-from snekql.errors import (
-    ModelDeclarationError as ModelDeclarationError,
-)
-from snekql.errors import (
-    ModelError as ModelError,
-)
-from snekql.errors import (
-    ModelValidationError as ModelValidationError,
-)
-from snekql.errors import (
-    PoolTimeoutError as PoolTimeoutError,
-)
-from snekql.errors import (
-    QueryCompilationError as QueryCompilationError,
-)
-from snekql.errors import (
-    QueryConstructionError as QueryConstructionError,
-)
-from snekql.errors import (
-    QueryError as QueryError,
-)
-from snekql.errors import (
-    SchemaError as SchemaError,
-)
-from snekql.errors import (
-    SchemaVerificationError as SchemaVerificationError,
-)
-from snekql.errors import (
-    SnekqlError as SnekqlError,
-)
-from snekql.errors import (
-    TransactionClosedError as TransactionClosedError,
-)
-from snekql.expressions import (
-    Assignment as Assignment,
-)
-from snekql.expressions import (
-    OrderBy as OrderBy,
-)
-from snekql.expressions import (
-    Predicate as Predicate,
-)
-from snekql.model import (
-    Col as Col,
-)
-from snekql.model import (
-    Fetched as Fetched,
-)
-from snekql.model import (
-    GenCol as GenCol,
-)
-from snekql.model import (
-    Model as Model,
-)
-from snekql.model import (
-    ModelMeta as ModelMeta,
-)
-from snekql.model import (
-    Pending as Pending,
-)
-from snekql.model import (
-    Table as Table,
-)
+from snekql.expressions import Assignment, OrderBy, Predicate
+from snekql.model import Col, Fetched, GenCol, Model, ModelMeta, Pending, Table
 from snekql.query import (
-    DeleteQuery as DeleteQuery,
+    DeleteQuery,
+    InsertQuery,
+    SelectModelQuery,
+    SelectTupleQuery,
+    SelectValueQuery,
+    UpdateQuery,
+    delete,
+    insert,
+    select,
+    update,
 )
-from snekql.query import (
-    InsertQuery as InsertQuery,
-)
-from snekql.query import (
-    SelectModelQuery as SelectModelQuery,
-)
-from snekql.query import (
-    SelectTupleQuery as SelectTupleQuery,
-)
-from snekql.query import (
-    SelectValueQuery as SelectValueQuery,
-)
-from snekql.query import (
-    UpdateQuery as UpdateQuery,
-)
-from snekql.query import (
-    delete as delete,
-)
-from snekql.query import (
-    insert as insert,
-)
-from snekql.query import (
-    select as select,
-)
-from snekql.query import (
-    update as update,
-)
-from snekql.runtime import Database as Database
-from snekql.runtime import Transaction as Transaction
+from snekql.runtime import Database, Transaction
 from snekql.storage import (
-    MISSING as MISSING,
-)
-from snekql.storage import (
-    Attr as Attr,
-)
-from snekql.storage import (
-    Blob as Blob,
-)
-from snekql.storage import (
-    Boolean as Boolean,
-)
-from snekql.storage import (
-    CurrentTimestamp as CurrentTimestamp,
-)
-from snekql.storage import (
-    DateTime as DateTime,
-)
-from snekql.storage import (
-    Integer as Integer,
-)
-from snekql.storage import (
-    Json as Json,
-)
-from snekql.storage import (
-    Missing as Missing,
-)
-from snekql.storage import (
-    Real as Real,
-)
-from snekql.storage import (
-    SchemaPolicy as SchemaPolicy,
-)
-from snekql.storage import (
-    Text as Text,
+    MISSING,
+    Attr,
+    Blob,
+    Boolean,
+    CurrentTimestamp,
+    DateTime,
+    Integer,
+    Json,
+    Missing,
+    Real,
+    SchemaPolicy,
+    Text,
 )
 
 __all__ = [
